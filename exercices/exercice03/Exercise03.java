b505e2a673a64826a5a57c2440ed379fb2df7fa6
Udriot-Paulin-EMF <paulin.udriot@studentfr.ch>
2024-08-29 11:45:02 +0200

    Save

---
+++ exercices/exercice03/Exercise03.java
@@ -0,0 +1,13 @@
+package exercices.exercice03;
+
+import javax.swing.plaf.basic.BasicOptionPaneUI.ButtonActionListener;
+
+public class Exercise03 {
+    public static void main(String[] args) {
+        byte age = 15;
+        String nom = "Paulin";
+        System.out.println("Je mappelle "+ nom + " et j'ai " + age + " ans");
+        int age2 = age + 10 ;
+        System.out.println("Dans 10 ans j'aurais "+ age2 + " ans");
+    }
+}
