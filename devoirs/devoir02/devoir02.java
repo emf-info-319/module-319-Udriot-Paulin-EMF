3e26b63885979c94ab084be49ba9f56fc08b2967
Udriot-Paulin-EMF <paulin.udriot@studentfr.ch>
2024-09-12 11:46:30 +0200

    savedevoir02

---
+++ devoirs/devoir02/devoir02.java
@@ -0,0 +1,9 @@
+package devoirs.devoir02;
+public class devoir02 {
+int CAPACITE_RESERVOIR_a = 3;
+int CAPACITE_RESERVOIR_B = 5;
+    public static void main(String[] args) {
+        
+    }
+    
+}
