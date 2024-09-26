2efcc5f3a237537aab9bc071895b9930547f25f3
Udriot-Paulin-EMF <paulin.udriot@studentfr.ch>
2024-09-26 11:01:56 +0200

    save

--- devoirs/devoir03/devoir03.java
+++ devoirs/devoir03/devoir03.java
@@ -2,16 +2,15 @@ package devoirs.devoir03;
 public class devoir03 {
     public static void main(String[] args) {
         int jour = 21;
-        int mois = 9;
+        int mois = 2;
         int annee = 2024;
-        int nummois = 0;
         int[] tab;
         tab =  new int[]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         if (annee % 4 == 0) {
-            tab[2] = tab [2] + 1;
+            tab[1] = tab [1] + 1;
         }  
         int nombreJourTotal = jour;
-        for (int i=0; i <mois-1; i++){
+        for (int i = 0; i < mois - 1; i++){
             //nombreJourTotal = nombreJourTotal + tab[i];
             nombreJourTotal += tab[i];
             
