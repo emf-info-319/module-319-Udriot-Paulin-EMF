a5325c04abb3d329aca7c08ecea34924f272e186
Udriot-Paulin-EMF <paulin.udriot@studentfr.ch>
2024-11-07 08:53:06 +0100

    save

---
+++ exercices/codagenround.java
@@ -0,0 +1,26 @@
+public class codagenround {
+    public final static double PI = 3.14;
+    public static void main(String[] args) {
+        int rayons[] = new int [10];
+        int max = 100;
+        int min = 0;
+        for (int j = 0; j < rayons.length; j++) {
+            rayons[j] = genreNombre(1, 10);
+            if(rayons[j] > 50){
+                System.out.println("Aire");          
+            }
+            else{
+                System.out.println("");
+            }
+        }
+        for (int i = rayons.length ; i > 0 ; i--){
+            System.out.println(rayons[i]);
+
+        }
+
+    }
+    public static int genreNombre(int min, int max){
+        int nombre = (int)(Math.random() * (max - min + 1))+min;
+        return nombre;
+    }
+}
