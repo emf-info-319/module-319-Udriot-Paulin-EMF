429bbeecd6b715e0e2746508f6095a684f1949d2
Paulin Udriot <paulin.udriot@studentfr.ch>
2024-11-14 13:05:00 +0100

    save

--- exercices/exerciceLivret.java
+++ exercices/exerciceLivret.java
@@ -1,16 +1,33 @@
+import java.util.Scanner;
+
 public class exerciceLivret {
+    
     public static final int MIN = 5;
     public static final int MAX = 20;
+    
 
     public static void main(String[] args) {
+        
         int nbr1 = choisirUnNombre();
         int nbr2 = choisirUnNombre();
         System.out.println(nbr1 + " " + nbr2);
         System.out.println();
+        System.out.println("entré votre ligne");
+        Scanner scanner = new Scanner(System.in);
+        int ligneDemande = scanner.nextInt();
+        if (ligneDemande> (nbr1*nbr2)) {
+            System.out.println("nombre pas possible");
+        }
+        scanner.nextLine();
+        
+        scanner.close();
+
         String [] tab = construireTableMultiplication(nbr1, nbr2);
+        
         for (int i = 0; i < tab.length; i++) {
           System.out.println(tab[i]);  
         }
+        System.out.println(tab[ligneDemande-1]);
     }
 
     public static int choisirUnNombre() {
@@ -24,15 +41,15 @@ public class exerciceLivret {
         int i = 1;
         int a = 1;
         int conteur = 0;
-        while (n2 != a-1) {
-            tab[conteur] = (i + " * " + a + " = " + i * a);
+        while (true) {
+            tab[conteur++] = (i + " * " + a + " = " + i * a);
             a++;
-            conteur++;
+            //conteur++;
             if (a -1 == n2 ) {
                 a = 1;
                 i++;
                 if (i == n1 + 1) {
-                    return tab;
+                   break;
                 }
             }
         }
--- exercices/testRp.java
+++ exercices/testRp.java
@@ -1,17 +1,8 @@
 public class testRp {
     public static void main(String[] args) {
-        int []tab = new int[] {1,2,3,4,5,6,7,8,9};
-        valeurMinimale(tab);
-        System.out.println(valeurMinimale(tab));
-    }
-     // Méthode pour trouver la valeur minimale dans un tableau
-     public static int valeurMinimale(int[] tableau) {
-        int min = tableau[0];
-        for (int i = 1; i < tableau.length; i++) {
-          if (min > tableau[i]) {
-            min = tableau[i];
-          }
-        }
-        return min;
-    }
+      String mot1 = "Java";
+      String mot2 = "java";
+      System.out.println(mot1.equals(mot2));  // Affiche : false
+          
+}
 }
